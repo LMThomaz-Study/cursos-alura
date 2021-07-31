15ed6a119a0a5469a3187bbe70b63f7de0c54cf4
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-07-31 18:38:54 -0300

    Início do problema

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import { Home } from './paginas/Home';
+import { Sobre } from './paginas/Sobre';
+
+import './assets/css/base/base.css';
 
 function App() {
-  return (
-  <>
+  const Router = () => {
+    const location = window.location.pathname;
 
-  </>
-  )
+    if (location === '/sobre') return <Sobre />;
+
+    return <Home />;
+  };
+
+  return <> {Router()} </>;
 }
 
-export default App
+export default App;
