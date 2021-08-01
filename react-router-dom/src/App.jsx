c4b5eec1d30b4d8ac8eaaf9dea1b49dbd26ba3a9
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-08-01 11:09:34 -0300

    Erro 404

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home } from './paginas/Home';
 import { Sobre } from './paginas/Sobre';
+import { Pagina404 } from './paginas/Pagina404';
 
 import './assets/css/base/base.css';
 
@@ -9,11 +10,14 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/'>
+        <Route exact path='/'>
           <Home />
         </Route>
         <Route path='/sobre'>
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </Router>
---
+++ react-router-dom/src/paginas/Pagina404.jsx
@@ -0,0 +1,12 @@
+import image from '../assets/img/doguito404.svg';
+
+import '../assets/css/404.css';
+
+export const Pagina404 = () => {
+  return (
+    <main className='container flex flex--centro flex--coluna'>
+      <img src={image} alt='Ilustração doguito' className='doguito-imagem' />
+      <p className='naoencontrado-texto'>Ops, essa página não existe!</p>
+    </main>
+  );
+};
