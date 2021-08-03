fe453224b47547b062f6cc60b1fe22636972b618
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-08-03 10:06:48 -0300

    Criando NavBar

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './paginas/Home';
 import { Sobre } from './paginas/Sobre';
 import { Pagina404 } from './paginas/Pagina404';
+import { Cabecalho } from './components/Cabecalho';
 
 import './assets/css/base/base.css';
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
