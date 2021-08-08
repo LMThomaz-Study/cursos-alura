125a7131b9d6b0dbd53c280fc423aeb8c42c8488
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-08-07 21:49:03 -0300

    Exibindo os posts

--- react-router-dom/src/paginas/Home.jsx
+++ react-router-dom/src/paginas/Home.jsx
@@ -1,9 +1,12 @@
+import { ListaPost } from '../components/ListaPost';
+
 export const Home = () => {
   return (
     <main>
       <div className='container'>
         <h2 className='titulo-pagina'>Pet notícias</h2>
       </div>
+      <ListaPost url={'/posts'} />
     </main>
   );
 };
