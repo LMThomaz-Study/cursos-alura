3e00be047b94705974ba0272ddfe3726bd8945c9
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-08-07 22:25:41 -0300

    Exibindo Post

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './paginas/Home';
 import { Sobre } from './paginas/Sobre';
 import { Pagina404 } from './paginas/Pagina404';
+import { Post } from './paginas/Post';
 import { Cabecalho } from './components/Cabecalho';
 
 import './assets/css/base/base.css';
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path='/sobre'>
           <Sobre />
+        </Route>
+        <Route path='/posts/:id'>
+          <Post />
         </Route>
         <Route>
           <Pagina404 />
--- react-router-dom/src/components/ListaPost.jsx
+++ react-router-dom/src/components/ListaPost.jsx
@@ -10,12 +10,14 @@ export const ListaPost = (props) => {
 
   useEffect(() => {
     busca(url, setPosts);
-  }, []);
+  }, [url]);
 
   return (
     <section className='posts container'>
       {posts.map((post) => (
-        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+        <Link
+          className={`cartao-post cartao-post--${post.categoria}`}
+          to={`/posts/${post.id}`}>
           <article key={post.id}>
             <h3 className='cartao-post__titulo'>{post.title}</h3>
             <p className='cartao-post__meta'>{post.metadescription}</p>
