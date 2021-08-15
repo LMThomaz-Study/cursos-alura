2860d631ccaf7f0f34ee57db78eb26dcd365228d
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-08-15 20:46:17 -0300

    O laço com while

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,23 +3,29 @@ print('Bem vindo no jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = 18
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input('Digite o seu número: ') 
+while (rodada <= total_de_tentativas):
+  print('Tentativa', rodada, ' de ', total_de_tentativas)
+  chute_str = input('Digite o seu número: ') 
 
-print('Você digitou ', chute_str)
+  print('Você digitou ', chute_str)
 
-chute = int(chute_str)
+  chute = int(chute_str)
 
-acertou = chute == numero_secreto
-chuteFoiMaior = chute > numero_secreto
-chuteFoiMenor = chute < numero_secreto
+  acertou = chute == numero_secreto
+  chuteFoiMaior = chute > numero_secreto
+  chuteFoiMenor = chute < numero_secreto
 
-if acertou:
-  print('Você acertou!')
-else:
-  if chuteFoiMaior:
-    print('Você errou! O seu chute foi maior do que o número secreto.')
-  elif chuteFoiMenor:
-    print('Você errou! O seu chute foi menor do que o número secreto.')
+  if acertou:
+    print('Você acertou!')
+  else:
+    if chuteFoiMaior:
+      print('Você errou! O seu chute foi maior do que o número secreto.')
+    elif chuteFoiMenor:
+      print('Você errou! O seu chute foi menor do que o número secreto.')
+
+  rodada = rodada + 1
 
 print('Fim de jogo!')
