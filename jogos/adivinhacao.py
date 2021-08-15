f10c70fe006e6f261e4ac39c882d586e5831b8b1
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-08-15 16:12:03 -0300

    A condição elif

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,15 +4,22 @@ print('*********************************')
 
 numero_secreto = 18
 
-chute_str = input('Digite o seu número: ')
+chute_str = input('Digite o seu número: ') 
 
 print('Você digitou ', chute_str)
 
 chute = int(chute_str)
 
-if (numero_secreto == chute):
+acertou = chute == numero_secreto
+chuteFoiMaior = chute > numero_secreto
+chuteFoiMenor = chute < numero_secreto
+
+if acertou:
   print('Você acertou!')
 else:
-  print('Você errou!')
+  if chuteFoiMaior:
+    print('Você errou! O seu chute foi maior do que o número secreto.')
+  elif chuteFoiMenor:
+    print('Você errou! O seu chute foi menor do que o número secreto.')
 
 print('Fim de jogo!')
