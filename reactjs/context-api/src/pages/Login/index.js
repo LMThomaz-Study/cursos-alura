220b7e1da234eeaf792b4142fc6a399b7b109257
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-13 08:15:03 -0300

    Criando as rotas

--- reactjs/context-api/src/pages/Login/index.js
+++ reactjs/context-api/src/pages/Login/index.js
@@ -1,50 +1,39 @@
 import { Button } from '@material-ui/core';
-import {
-  Container,
-  Titulo,
-  InputContainer
-} from './styles';
-import {
-  Input,
-  InputLabel,
-  InputAdornment 
-} from '@material-ui/core';
+import { Container, Titulo, InputContainer } from './styles';
+import { Input, InputLabel, InputAdornment } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-function Login() {
+function Login({ nome, setNome, saldo, setSaldo }) {
+  const history = useHistory();
+
   return (
     <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
+      <Titulo>Insira o seu nome</Titulo>
       <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
+        <InputLabel>Nome</InputLabel>
         <Input
-          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          type='text'
         />
       </InputContainer>
       <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
+        <InputLabel>Saldo</InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          value={saldo}
+          onChange={(e) => setSaldo(e.target.value)}
+          type='number'
+          startAdornment={<InputAdornment position='start'>R$</InputAdornment>}
+        />
       </InputContainer>
       <Button
-        variant="contained"
-        color="primary"
-      >
+        variant='contained'
+        color='primary'
+        onClick={() => history.push('/feira')}>
         Avançar
       </Button>
     </Container>
-  )
-};
+  );
+}
 
 export default Login;
