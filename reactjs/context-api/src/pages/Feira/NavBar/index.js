ca762b08e829b5305f68cb312adee069e56e6a67
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-14 09:39:46 -0300

    Abrindo a tela de carrinho

--- reactjs/context-api/src/pages/Feira/NavBar/index.js
+++ reactjs/context-api/src/pages/Feira/NavBar/index.js
@@ -1,17 +1,21 @@
-import { Nav } from './styles';
-import { ReactComponent as Logo } from 'assets/logo.svg';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { ReactComponent as Logo } from 'assets/logo.svg';
 import { useCarrinhoContext } from 'common/context/Carrinho';
+import { useHistory } from 'react-router';
+import { Nav } from './styles';
 
 export default function NavBar() {
   const { quantidadeTotal } = useCarrinhoContext();
+  const history = useHistory();
 
   return (
     <Nav>
       <Logo />
-      <IconButton disabled={!quantidadeTotal}>
+      <IconButton
+        disabled={!quantidadeTotal}
+        onClick={() => history.push('/carrinho')}>
         <Badge color='primary' badgeContent={quantidadeTotal}>
           <ShoppingCartIcon />
         </Badge>
