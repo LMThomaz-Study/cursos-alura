4979d5a37649668a6c6749403d1ad1cb89557020
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-15 19:58:40 -0300

    Aplicando validações de entrada

--- nodejs/express-mysql/models/atendimentos.js
+++ nodejs/express-mysql/models/atendimentos.js
@@ -3,10 +3,30 @@ const conexao = require('../infra/conexao');
 
 class Atendimento {
   adicionar(atendimento, res) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:mm:SS');
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
     const data = moment(atendimento.data, 'DD/MM/YYYY').format(
       'YYYY-MM-DD HH:MM:SS',
     );
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
+
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual',
+      },
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos 5 caracteres',
+      },
+    ];
+
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length > 0;
+
+    if (existemErros) return res.status(400).json(erros);
 
     const atendimentoTratado = { dataCriacao, ...atendimento, data };
     const sql = 'INSERT INTO Atendimentos SET ?';
