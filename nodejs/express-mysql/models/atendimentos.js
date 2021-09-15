17b5aed14b7de6e1c19101ad896004853dceaa58
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-15 19:47:22 -0300

    Adicionando data no atendimento

--- nodejs/express-mysql/models/atendimentos.js
+++ nodejs/express-mysql/models/atendimentos.js
@@ -1,10 +1,17 @@
+const moment = require('moment');
 const conexao = require('../infra/conexao');
 
 class Atendimento {
   adicionar(atendimento) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format(
+      'YYYY-MM-DD HH:MM:SS',
+    );
+
+    const atendimentoTratado = { dataCriacao, ...atendimento, data };
     const sql = 'INSERT INTO Atendimentos SET ?';
 
-    conexao.query(sql, atendimento, (erro, resultado) => {
+    conexao.query(sql, atendimentoTratado, (erro, resultado) => {
       if (erro) {
         console.log(erro);
         return;
