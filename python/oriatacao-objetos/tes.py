14b778956e301e5d0e33ebf63f342bc2c641b3e3
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-17 19:27:54 -0300

    Dados e comportamento

--- python/oriatacao-objetos/tes.py
+++ python/oriatacao-objetos/tes.py
@@ -2,5 +2,18 @@ def cria_conta(numero, titular, saldo, limite):
   conta = {'numero': numero, 'titular': titular, 'saldo': saldo, 'limite': limite}
   return conta
 
-conta = cria_conta(123, 'Jose', 100.0, 1000.0)
-print(conta)
+def deposita(conta, valor):
+  conta['saldo'] += valor
+
+def saca(conta, valor):
+  conta['saldo'] -= valor
+
+def extrato(conta):
+  print('Saldo de {}'.format(conta['saldo']))
+
+conta = cria_conta(123, 'José', 1500.0, 1000.0)
+deposita(conta, 500.0)
+extrato(conta)
+
+saca(conta, 200.0)
+extrato(conta)
