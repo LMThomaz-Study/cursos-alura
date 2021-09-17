458656f38a63ac48deb75ab402ea9f29590c4a7e
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-17 05:15:38 -0300

    Segurança ao receber dados externos

--- nodejs/streaming-repositorio/arquivos/uploadDeArquivos.js
+++ nodejs/streaming-repositorio/arquivos/uploadDeArquivos.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (pathName, nomeDoArquivo, callback) => {
-  const novoPathName = `../assets/images/${nomeDoArquivo}`;
+  const tiposValido = ['jpg', 'jpeg', 'png', 'jfif'];
+  const tipo = path.extname(pathName);
+  const tipoEhValido = tiposValido.indexOf(tipo.substring(1));
+
+  if (tipoEhValido === -1) {
+    console.log('Erro! Tipo inválido');
+    return;
+  }
+
+  const novoPathName = `./assets/images/${nomeDoArquivo}${tipo}`;
 
   fs.createReadStream(pathName)
     .pipe(fs.createWriteStream(novoPathName))
