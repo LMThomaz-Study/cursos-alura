a1d47e9dc558827c1f075516a949806e847e6bf2
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-18 07:54:32 -0300

    Usando métodos

--- python/oriatacao-objetos/conta.py
+++ python/oriatacao-objetos/conta.py
@@ -1,11 +1,24 @@
 class Conta:
   def __init__(self, numero, titular, saldo, limite):
+    print('Conta criada - {}'.format(self))
     self.numero = numero
     self.titular = titular
     self.saldo = saldo
     self.limite = limite
-    print('Conta criada - {}'.format(self))
+    
+  def extrato(self):
+    print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+    
+  def depositar(self, valor):
+    self.saldo += valor
+    
+  def sacar(self, valor):
+    self.saldo -= valor
 
 
 conta = Conta(123, 'Pedro', 100.0, 1000.0)
 print(conta.titular)
+conta.depositar(100.0)
+conta.extrato()
+conta.sacar(50.0)
+conta.extrato()
