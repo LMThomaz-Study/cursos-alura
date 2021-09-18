6eda838f0df4dc0a0258608045a577423e0eba09
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-17 21:07:05 -0300

    Construtor

--- python/oriatacao-objetos/conta.py
+++ python/oriatacao-objetos/conta.py
@@ -1,2 +1,10 @@
 class Conta:
-  pass
+  def __init__(self, numero, titular, saldo, limite):
+    self.numero = numero
+    self.titular = titular
+    self.saldo = saldo
+    self.limite = limite
+    print('Conta criada - {}'.format(self))
+
+
+conta = Conta(123, 'Pedro', 100.0, 1000.0)
