f59080d64d1b9c0aebe6ad3182f0da4a54eeb0fa
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-18 09:19:17 -0300

    Atributos privados

--- python/oriatacao-objetos/conta.py
+++ python/oriatacao-objetos/conta.py
@@ -1,23 +1,23 @@
 class Conta:
   def __init__(self, numero, titular, saldo, limite):
     print('Conta criada - {}'.format(self))
-    self.numero = numero
-    self.titular = titular
-    self.saldo = saldo
-    self.limite = limite
+    self.__numero = numero
+    self.__titular = titular
+    self.__saldo = saldo
+    self.__limite = limite
     
   def extrato(self):
-    print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+    print('Saldo de {} do titular {}'.format(self.__saldo, self.__titular))
     
   def depositar(self, valor):
-    self.saldo += valor
+    self.__saldo += valor
     
   def sacar(self, valor):
-    self.saldo -= valor
+    self.__saldo -= valor
 
 
 conta = Conta(123, 'Pedro', 100.0, 1000.0)
-print(conta.titular)
+
 conta.depositar(100.0)
 conta.extrato()
 conta.sacar(50.0)
