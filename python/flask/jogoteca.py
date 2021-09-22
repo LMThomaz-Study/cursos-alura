0ded6fef84796d773c0f61a65ab78ee44398601b
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-22 13:05:29 -0300

    Redirecionando para outra página

--- python/flask/jogoteca.py
+++ python/flask/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -34,7 +34,7 @@ def criar():
     console = request.form["console"]
     jogo = Jogo(nome, categoria, console)
     lista_jogos.append(jogo)
-    return render_template("lista.html", titulo="Jogos", jogos=lista_jogos)
+    return redirect("/")
 
 
 app.run(debug=True)
