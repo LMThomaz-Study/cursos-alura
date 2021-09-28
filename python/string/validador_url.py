de5985766ea52f07243672426853ea8e8e47fdff
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-27 23:16:22 -0300

    Validando nossa URL com RegEx

---
+++ python/string/validador_url.py
@@ -0,0 +1,11 @@
+import re
+
+url = "https://www.bytebank.com.br/cambio"
+
+padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError("URL inválida")
+
+print("A URL é válida")
