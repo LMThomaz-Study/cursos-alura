206111506b1c9491ca752010b458d591279e58c2
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-09-27 22:18:12 -0300

    Quantificadores e intervalos

--- python/string/extrator_cep.py
+++ python/string/extrator_cep.py
@@ -1,10 +1,11 @@
 import re  # Regular Expression - RegEx
 
-endereco = "Avenida Brasil da Moca 987, 10000100, Casa, Centro, Buntabo, SP"
+endereco = "Avenida Brasil da Moca 987, 10000-100, Casa, Centro, Buntabo, SP"
 
-# 5 dígitos + hífen (opcional) + 3 dígitos
-padrao = re.compile(
-    "[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789]")
+# 5 dígitos de 0 até 9 + hífen (opcional) + 3 dígitos de 0 até 9
+# padrao = re.compile("[0-9]{5}[-]?[0-9]{3}")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
+
 busca = padrao.search(endereco)  # Match or return None
 
 if busca:
